ce9b070f639a7fc956eecfb781d3dfe1a52603eb
Wilkware <heiko.wilknitz@wilkware.de>
2025-08-02 13:00:08 +0200

    Update v3.1.20250802

--- libs/VersionHelper.php
+++ libs/VersionHelper.php
@@ -52,7 +52,7 @@ trait VersionHelper
      * Check, if given instance id of type tile visu
      *
      * @param int $iid Instance ID
-     * 
+     *
      * @return bool TRUE if match the desired type, otherwise FALSE.
      */
     protected function IsTileVisuInstance(int $iid): bool
@@ -73,7 +73,7 @@ trait VersionHelper
      * Check, if given instance id of type webfront visu
      *
      * @param int $iid Instance ID
-     * 
+     *
      * @return bool TRUE if match the desired type, otherwise FALSE.
      */
     protected function IsWebFrontVisuInstance(int $iid): bool
